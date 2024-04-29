ab07f4c1ec3eb3f8ff9a6b3b71e9b38739be062f
Kiêm Khang <150389500+Cutecatttt@users.noreply.github.com>
2024-04-29 15:35:17 +0700

    Update String.cpp

--- String.cpp
+++ String.cpp
@@ -8,26 +8,26 @@ Trước hàm getline() mà trong luồng cin có thừa kí tự enter thì ph�
 Để nối 2 xâu bạn có thể dùng toán tử +=
 Để so sánh 2 xâu bạn có thể sử dụng luôn các toán tử so sánh như >, <, !=, ==...
 
-Hàm length() và size() Trả về độ dài xâu
-Hàm push_back()        Thêm 1 ký tự vào cuối string 
-Hàm pop_back()         Xóa đi ký tự cuối cùng trong string.
-Hàm insert()           Chèn một xâu khác vào vị trí bất kỳ trong xâu hiện tại thông qua chỉ số chèn.
-Hàm erase(pos, n)      Xóa n ký tự trong xâu s tính từ chỉ số pos
-Hàm substr(pos, n)     Trả về xâu con gồm n ký tự tính từ chỉ số pos của xâu s
-Hàm find()             Tìm kiếm sự xuất hiện của xâu con trong xâu hiện tại,
-<hàm sẽ trả về chỉ số đầu tiên của xâu con nếu xâu này tồn tại trong xâu đang tìm kiếm, ngược lại sẽ trả về giá trị là string::npos>
-Hàm a.compare(b)       So sánh 2 xâu a và b,
+s.length() và s.ize() Trả về độ dài xâu.  <ĐPT O(1)>
+s.push_back(a):Thêm ký tự a vào cuối string . <ĐPT O(1)>
+s.pop_back(): Xóa đi ký tự cuối cùng trong string. <ĐPT O(1)>
+s.insert(index, string t): Chèn xâu t vào vị trí bất kỳ trong xâu hiện tại thông qua chỉ số index. <ĐPT O(n)>
+s.erase(pos, n): Xóa n ký tự trong xâu s tính từ chỉ số pos. <ĐPT O(n)>
+s.substr(pos, n): Trả về xâu con gồm n ký tự tính từ chỉ số pos của xâu s. <ĐPT O(n)>
+s.find(t): Tìm kiếm sự xuất hiện của xâu con t trong xâu hiện tại.  <ĐPT O(n)>
+<Hàm sẽ trả về chỉ số đầu tiên của xâu con nếu xâu này tồn tại trong xâu đang tìm kiếm, ngược lại sẽ trả về giá trị là string::npos>
+a.compare(b): So sánh 2 xâu a và b.
 <Hàm trả về 0 khi hai xâu bằng nhau, trả về -1 nêu xâu a nhỏ hơn xâu b và trả về 1 nếu xâu a lớn hơn xâu b>
-Hàm tolower()          Chuyển các chữ về chữ in thường
-Hàm toupper()          Chuyển các chữ về chữ in hoa
-Hàm stringstream       Biến xâu string của bạn thành luồng (tương tự như luồng vào từ bàn phím là cin)
+tolower(s[index]): Chuyển chữ về chữ in thường. <ĐPT O(1)>
+toupper(s[index]): Chuyển chữ về chữ in hoa.  <ĐPT O(1)>
+stringstream ss(s): Biến xâu string s thành luồng (tương tự như luồng vào từ bàn phím là cin)
 
-Hàm stoi()	 Chuyển string thành số int
-Hàm stoll()      Chuyển string thành số long long
-Hàm stof()       Chuyển string thành số float
-Hàm stod()	 Chuyển string thành số double
-Hàm stold()      Chuyển string thành số long double
-Hàm to_string()  Chuyển số thành xâu.
+stoi(s)       Chuyển string thành số int
+stoll(s)      Chuyển string thành số long long
+stof(s)       Chuyển string thành số float
+stod(s)	      Chuyển string thành số double
+stold(s)      Chuyển string thành số long double
+to_string(n)  Chuyển số thành xâu
 */
 void email_1(string n, string s){
 	s += ".";
