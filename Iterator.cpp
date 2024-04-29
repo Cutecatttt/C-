3cb47dac27ec715c6e53a885756ebc81325e22c9
Kiêm Khang <150389500+Cutecatttt@users.noreply.github.com>
2024-04-30 01:04:29 +0700

    Create Iterator.cpp

---
+++ Iterator.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+<vector/set/map><data_type>::iterator iterator_name;
+Iterator tương tự như con trỏ, nó trỏ tới các phần tử trong vector, set, map,...
+begin() : Iterator trỏ tới phần tử đầu tiên 
+end() : Iterator trỏ tới phần tử sau phần tử cuối cùng 
+rbegin() : Iterator ngược trỏ tới phần tử cuối cùng
+rend() : Iterator ngược trỏ tới phần tử trước phần tử đầu tiên
+*/
