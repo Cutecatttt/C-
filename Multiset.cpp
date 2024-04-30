c189121ee2659f733d52b28e072ba0584a682660
Kiêm Khang <150389500+Cutecatttt@users.noreply.github.com>
2024-04-30 12:09:39 +0700

    Update Multiset.cpp

--- Multiset.cpp
+++ Multiset.cpp
@@ -5,8 +5,8 @@ using namespace std;
 multiset<data_type> set_name;
 Tương tự như set nhưng các phần tử trong multiset có thể trùng nhau
 Sử dụng các hàm tương tự như trong set
-s.erase(a): Xóa đi các phần tử có cùng giá trị a. => ĐPT(logN)
-s.erase(iterator): xóa đi phần tử mà iterator đang chỉ tới. => ĐPT(logN)
+s.erase(a): Xóa đi các phần tử có cùng giá trị a. => ĐPT O(N)
+s.erase(iterator): xóa đi phần tử mà iterator đang chỉ tới. => ĐPT O(1)
 */
 int main() {
     //cho mang n phan tu va so nguyen k
